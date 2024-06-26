4ce7d4391d93d6a414738247b61ed676b7bd8c9f
IngM5058 <153575241+IngM5058@users.noreply.github.com>
2024-06-27 00:24:01 +0700

    change inventory stock update methord
    add condition to display stock

--- main.js
+++ main.js
@@ -6,11 +6,13 @@ createApp({
         const description = ref('These Socks is quite green.');
         const image = ref('./assets/images/socks_green.jpg');
         const inStock = ref(true);
+        const inventory = ref(100);
             return {
                 product,
                 description,
                 image,
                 inStock,
+                inventory,
             };
 
     },
