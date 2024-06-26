fda37314d9821ea7c2f52341734640db2334feef
IngM5058 <153575241+IngM5058@users.noreply.github.com>
2024-06-26 23:44:03 +0700

    added product image
    add green socks image with v-bind

--- main.js
+++ main.js
@@ -2,11 +2,13 @@ const { createApp, ref } = Vue;
 
 createApp({
     setup() {
-        const product = ref('Boots');
-        const description = ref('These Boots quite dirty');
+        const product = ref('Socks');
+        const description = ref('These Socks is quite green.');
+        const image = ref('./assets/images/socks_green.jpg');
             return {
                 product,
-                description
+                description,
+                image,
             };
 
     },
