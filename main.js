fcd347f7b7d957e86994c059340982272a486d2b
IngM5058 <153575241+IngM5058@users.noreply.github.com>
2024-06-27 00:37:49 +0700

    add onSale display
    according to 4.9

--- main.js
+++ main.js
@@ -7,12 +7,14 @@ createApp({
         const image = ref('./assets/images/socks_green.jpg');
         const inStock = ref(true);
         const inventory = ref(100);
+        const onSale = ref(true);
             return {
                 product,
                 description,
                 image,
                 inStock,
                 inventory,
+                onSale,
             };
 
     },
