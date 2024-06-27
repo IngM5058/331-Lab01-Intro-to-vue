c3c733029b6efe95f0368009a92365b72d237afe
IngM5058 <153575241+IngM5058@users.noreply.github.com>
2024-06-27 22:37:56 +0700

    till 8.3 compute

--- main.js
+++ main.js
@@ -5,8 +5,6 @@ createApp({
         const product = ref('Socks');
         const brand =ref('SE 331')
         const description = ref('Bro, trust me, dont buy it.');
-        const image = ref('./assets/images/socks_green.jpg');
-        const inStock = ref(true);
         function updateStock() {
             inStock.value = !inStock.value;
         }
@@ -21,11 +19,24 @@ createApp({
         ]);
 
         const variants = ref([
-            { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-            { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+            { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50},
+            { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0},
         ]);
         function updateImage(variantImage) {
             image.value = variantImage;
+        }
+
+        const image = computed(() => {
+            return variants.value[selectedVariant.value].image;
+        });
+
+        const inStock = computed(() => {
+            return variants.value[selectedVariant.value].quantity;
+        });
+
+        const selectedVariant = ref(0);
+        function updateVariant(index) {
+            selectedVariant.value = index;
         }
 
         const sizes = ref([
