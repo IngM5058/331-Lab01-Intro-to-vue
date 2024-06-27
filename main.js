83116d563d50db1aadb00c3bd827ea9b2bca1836
IngM5058 <153575241+IngM5058@users.noreply.github.com>
2024-06-27 20:04:35 +0700

    add details. colors, sizes

--- main.js
+++ main.js
@@ -8,6 +8,22 @@ createApp({
         const inStock = ref(true);
         const inventory = ref(100);
         const onSale = ref(true);
+        const details = ref([
+            '50% cotton',
+            '30% wool',
+            '20% polyester'
+        ]);
+        const variants = ref([
+            { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+            { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+        ]);
+
+        const sizes = ref([
+            { id: 1, size: 'S' },
+            { id: 2, size: 'M' },
+            { id: 3, size: 'L' },
+        ]);
+
             return {
                 product,
                 description,
@@ -15,7 +31,9 @@ createApp({
                 inStock,
                 inventory,
                 onSale,
+                details,
+                variants,
+                sizes,
             };
-
     },
 }).mount('#app');
