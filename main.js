ba9d4e52674f4a303694b0c9d972ee42433f5217
IngM5058 <153575241+IngM5058@users.noreply.github.com>
2024-07-09 22:17:45 +0700

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const { createApp, ref, computed, reactive, toRefs } = Vue;
 const app = createApp({
     setup() {
         const cart = ref([]);
-        const premium = ref(false);
+        const premium = ref(true);
         const details = ref(["50% cotton", "30% wool", "20% polyester"]);
         function updateCart(_id) {
             for (var i = 0; i < cart.value.length; i++) {
